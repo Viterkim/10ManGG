f1c668c2ff3b6561ea5603f6e634f01b75e4fb0b
Viktor Kim Christiansen <Viktor Kim Christiansen>
2017-01-17 14:54:35 +0100

    Bingo

--- 10Man/src/java/control/ControllerInterface.java
+++ 10Man/src/java/control/ControllerInterface.java
@@ -7,14 +7,19 @@ import entity.Player;
 import entity.PlayerOverview;
 
 
+/*
+Forsvind
+*/
 public interface ControllerInterface {
     public HistoryOverview getHistoryOverview(int matchId);
     
     public Match getMatch(int matchId);
+    public boolean isMatchDone();
     
     public PlayerOverview getPlayerOverview(int matchId);
     public PlayerOverview getPlayersAvailable(int matchId);
     public Player getPlayer(int playerId);
+    
     
     
     
